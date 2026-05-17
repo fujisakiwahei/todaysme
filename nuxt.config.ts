f3ccc0d7c8cef3dd164bf3c7684316111596fcf0
Wahei's MacBook Air <fujisakiwahei@gmail.com>
2026-05-17 16:07:54 +0900

    supabaseのセットアップ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
+      supabasePublishableKey: process.env.NUXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    },
+  },
+
   modules: ["@pinia/nuxt", "@sentry/nuxt/module", "@nuxt/eslint"],
 
   css: ["~/assets/styles/style.scss"],
