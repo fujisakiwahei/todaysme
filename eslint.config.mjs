e36782eb7a4da9e088d178ec38a448002e54fe7d
Wahei's MacBook Air <fujisakiwahei@gmail.com>
2026-07-15 23:44:09 +0900

    セルフクロージングするタグを許容

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt();
-// Your custom configs here
+export default withNuxt({
+  rules: {
+    "vue/html-self-closing": [
+      "warn",
+      {
+        html: {
+          void: "always", // <img />
+          normal: "always",
+          component: "always",
+        },
+
+        svg: "always",
+        math: "always",
+      },
+    ],
+  },
+});
